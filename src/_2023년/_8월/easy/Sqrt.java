8026beaf8bd60d917a4932b964bf5caf0ca4d3a9
gusals <gusals6586@gmail.com>
2023-08-06 22:46:25 +0900

    8/6

---
+++ src/_2023년/_8월/easy/Sqrt.java
@@ -0,0 +1,24 @@
+package _2023년._8월.easy;
+
+public class Sqrt {
+    public int mySqrt(int x) {
+        if(x <= 1)
+            return x;
+
+        int start = 1;
+        int end = x/2;
+
+        while(start <= end) {
+            int mid = (start+end)/2;
+
+            if(x / mid == mid) {
+                return mid;
+            } else if(mid >  x / mid) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return end;
+    }
+}
