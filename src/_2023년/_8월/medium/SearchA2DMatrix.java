d2a2fa4851c52e92a9937cf1c3369a4be4304447
gusals <gusals6586@gmail.com>
2023-08-16 19:46:19 +0900

    8/16

---
+++ src/_2023년/_8월/medium/SearchA2DMatrix.java
@@ -0,0 +1,25 @@
+package _2023년._8월.medium;
+
+public class SearchA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = (left + right)/2;
+            int val = matrix[mid / cols][mid % cols];
+
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
