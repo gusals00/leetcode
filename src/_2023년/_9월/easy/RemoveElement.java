7de39099944c0b154f22f1bbce534887c39d55aa
gusals <gusals6586@gmail.com>
2023-09-03 11:08:28 +0900

    9/3

---
+++ src/_2023년/_9월/easy/RemoveElement.java
@@ -0,0 +1,13 @@
+package _2023년._9월.easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int result = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[result++] = nums[i];
+            }
+        }
+        return result;
+    }
+}
