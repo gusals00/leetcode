3b22638dffa37eddb5d6b01e7470ac072a5dcf8d
gusals <gusals6586@gmail.com>
2023-09-06 20:15:55 +0900

    9/6

---
+++ src/_2023년/_9월/easy/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package _2023년._9월.easy;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
