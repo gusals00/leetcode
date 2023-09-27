8c682262d621500164a24cba477267cc7cf37ecc
gusals <gusals6586@gmail.com>
2023-09-27 23:25:56 +0900

    9/27

---
+++ src/_2023년/_9월/easy/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+package _2023년._9월.easy;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = (start+end)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+
+        return end;
+    }
+}
