e65bc1ccaa8a0db07100c0ca23bf426df2349539
gusals <gusals6586@gmail.com>
2023-10-01 20:06:05 +0900

    10/1

---
+++ src/_2023년/_10월/medium/PartitionList.java
@@ -0,0 +1,42 @@
+package _2023년._10월.medium;
+
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessHead = new ListNode(0);
+        ListNode greaterHead = new ListNode(0);
+        ListNode less = lessHead;
+        ListNode greater = greaterHead;
+
+        while(head != null) {
+            if(head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+
+        less.next = greaterHead.next;
+        greater.next = null;
+        return lessHead.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
