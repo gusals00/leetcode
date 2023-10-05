88e66d83f0d710629f58667a698b35125d156c8c
gusals <gusals6586@gmail.com>
2023-10-05 23:33:18 +0900

    10/5

---
+++ src/_2023년/_10월/medium/BSTIterator.java
@@ -0,0 +1,55 @@
+package _2023년._10월.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BSTIterator {
+
+    private Deque<TreeNode> deque;
+
+    public BSTIterator(TreeNode root) {
+        deque = new ArrayDeque<>();
+        while(root != null) {
+            deque.push(root);
+            root = root.left;
+        }
+    }
+
+    public int next() {
+        TreeNode pop = deque.pop();
+        int val = pop.val;
+
+        if(pop.right != null) {
+            pop = pop.right;
+            while(pop != null) {
+                deque.push(pop);
+                pop = pop.left;
+            }
+        }
+
+        return val;
+    }
+
+    public boolean hasNext() {
+        return !deque.isEmpty();
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
