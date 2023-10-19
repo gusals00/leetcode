d16e9d702658dd95f97e8fba89cf009c3a6e951c
gusals <gusals6586@gmail.com>
2023-10-19 20:45:53 +0900

    10/19

---
+++ src/_2023년/_10월/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,38 @@
+package _2023년._10월.easy;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length-1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if(left > right)
+            return null;
+
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, left, mid-1);
+        node.right = sortedArrayToBST(nums, mid+1, right);
+
+        return node;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
