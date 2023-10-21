95a99c09cee58fd39b0cadd8d2e6942f8f5aae07
gusals <gusals6586@gmail.com>
2023-10-21 21:31:20 +0900

    10/21

---
+++ src/_2023년/_10월/medium/SearchInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+package _2023년._10월.medium;
+
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if(nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
