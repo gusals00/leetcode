dc5b131cbb90a5e15cdeea07054bb6d60c817446
gusals <gusals6586@gmail.com>
2023-10-24 22:24:04 +0900

    10/24

---
+++ src/_2023년/_10월/easy/NumberOf1Bits.java
@@ -0,0 +1,15 @@
+package _2023년._10월.easy;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int result = 0;
+        for(int i=0; i<32; i++) {
+            if((n & 1) == 1)
+                result++;
+
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
---
+++ src/_2023년/_10월/easy/SingleNumber.java
@@ -0,0 +1,12 @@
+package _2023년._10월.easy;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
