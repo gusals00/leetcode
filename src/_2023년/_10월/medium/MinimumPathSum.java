e46e35dc28e5a0f79d689fd34fda571ba9a886da
gusals <gusals6586@gmail.com>
2023-10-31 23:51:40 +0900

    10/31

---
+++ src/_2023년/_10월/medium/MinimumPathSum.java
@@ -0,0 +1,21 @@
+package _2023년._10월.medium;
+
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        for(int i=1; i<grid[0].length; i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+
+        for(int i=1; i<grid.length; i++) {
+             grid[i][0] += grid[i-1][0];
+        }
+
+        for(int i=1; i<grid.length; i++) {
+            for(int j=1; j<grid[i].length; j++) {
+                grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
+            }
+        }
+
+        return grid[grid.length-1][grid[0].length-1];
+    }
+}
