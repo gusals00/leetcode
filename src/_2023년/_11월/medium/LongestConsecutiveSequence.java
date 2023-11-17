1fee2d17a5816d3df02ca607bfaa130322b18d64
gusals <gusals6586@gmail.com>
2023-11-17 22:37:44 +0900

    11/17

---
+++ src/_2023년/_11월/medium/LongestConsecutiveSequence.java
@@ -0,0 +1,40 @@
+package _2023년._11월.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int result = 0;
+
+        for(int n : nums)
+            set.add(n);
+
+        for(int n : nums) {
+            int cnt = 0;
+
+            if(set.contains(n)) {
+                set.remove(n);
+                cnt++;
+
+                int left = n-1;
+                int right = n+1;
+
+                while(set.contains(left)) {
+                    set.remove(left);
+                    cnt++;
+                    left--;
+                }
+                while(set.contains(right)) {
+                    set.remove(right);
+                    cnt++;
+                    right++;
+                }
+            }
+            result = Math.max(result, cnt);
+        }
+
+        return result;
+    }
+}
