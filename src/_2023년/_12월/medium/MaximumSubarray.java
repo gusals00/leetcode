78a6c01eb5dbea118256e24ecea86348ddcebdf0
gusals <gusals6586@gmail.com>
2023-12-14 23:29:23 +0900

    12/14

---
+++ src/_2023년/_12월/medium/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package _2023년._12월.medium;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = nums[0];
+        dp[0] = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1]+nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
