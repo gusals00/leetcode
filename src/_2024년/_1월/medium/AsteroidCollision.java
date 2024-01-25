2c995b0b64112dcbe8176ef07930494fd97274d8
gusals <gusals6586@gmail.com>
2024-01-25 23:11:35 +0900

    1/25

---
+++ src/_2024년/_1월/medium/AsteroidCollision.java
@@ -0,0 +1,33 @@
+package _2024년._1월.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                }
+                if (stack.peek() + asteroid == 0) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for(int i=0; i<result.length; i++) {
+            result[i] = stack.removeLast();
+        }
+
+        return result;
+    }
+}
---
+++ src/_2024년/_1월/medium/RemovingStarsFromAString.java
@@ -0,0 +1,16 @@
+package _2024년._1월.medium;
+
+public class RemovingStarsFromAString {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if(c == '*') {
+                sb.deleteCharAt(sb.length()-1);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
