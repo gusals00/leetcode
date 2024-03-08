076b7b92abe1ffaf718517682adde5b6c2e126ce
gusals <gusals6586@gmail.com>
2024-03-08 16:57:08 +0900

    3/8

---
+++ src/_2024년/_3월/easy/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+package _2024년._3월.easy;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
