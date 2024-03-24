b9493ca662f623c2ee6ecc80979adfb4d0908c61
gusals <gusals6586@gmail.com>
2024-03-24 16:15:47 +0900

    3/24

---
+++ src/_2024년/_3월/medium/IncreasingTripletSubsequence.java
@@ -0,0 +1,20 @@
+package _2024년._3월.medium;
+
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] <= min) {
+                min = nums[i];
+            } else if(nums[i] <= secondMin) {
+                secondMin = nums[i];
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
