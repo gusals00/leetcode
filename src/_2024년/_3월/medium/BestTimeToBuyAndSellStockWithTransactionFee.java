8626c57799c56b4a3434b13492c29a0f1929e5fc
gusals <gusals6586@gmail.com>
2024-03-28 15:37:59 +0900

    3/28

---
+++ src/_2024년/_3월/medium/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,15 @@
+package _2024년._3월.medium;
+
+public class BestTimeToBuyAndSellStockWithTransactionFee {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = -prices[0];
+        int sell = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            buy = Math.max(buy, sell - prices[i]);
+            sell = Math.max(sell, prices[i] + buy - fee);
+        }
+
+        return sell;
+    }
+}
