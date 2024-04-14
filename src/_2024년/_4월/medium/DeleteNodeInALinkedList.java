ef4184aaeacd54f1c7531f6b1276babf99010465
gusals <gusals6586@gmail.com>
2024-04-14 23:26:17 +0900

    4/14

---
+++ src/_2024년/_4월/medium/DeleteNodeInALinkedList.java
@@ -0,0 +1,17 @@
+package _2024년._4월.medium;
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
