c58f51f222fa4e5daa2fc9a95b74276ced4a59e4
gusals <gusals6586@gmail.com>
2024-06-13 18:39:10 +0900

    6/13

---
+++ src/_2024년/_6월/programmers/lv2/멀리_뛰기.java
@@ -0,0 +1,21 @@
+package _2024년._6월.programmers.lv2;
+
+public class 멀리_뛰기 {
+    public long solution(int n) {
+        if(n == 1) {
+            return 1;
+        } else if(n == 2) {
+            return 2;
+        }
+
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] += (dp[i-1] + dp[i-2]) % 1234567;
+        }
+
+        return dp[n];
+    }
+}
