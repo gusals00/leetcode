4a9b5d4298108ad2aa3b2a0815c4832864a3b41b
gusals <gusals6586@gmail.com>
2024-10-24 18:51:03 +0900

    10/24

---
+++ src/_2024년/_10월/programmers/lv2/피보나치_수.java
@@ -0,0 +1,15 @@
+package _2024년._10월.programmers.lv2;
+
+public class 피보나치_수 {
+    public int solution(int n) {
+        int[] fibo = new int[n+1];
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i=2; i<=n; i++) {
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        }
+
+        return fibo[n];
+    }
+}
